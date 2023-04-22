4b45c6794d51ced661c2be796b93d519deedf7e7
Hannah Sloan <hannah.ava.sloan@gmail.com>
2023-04-22 14:05:28 -0600

    Sub-home pages! A ship section and a GM section

--- markdownToHtml.py
+++ markdownToHtml.py
@@ -1,4 +1,4 @@
-import sys
+import os
 
 def replaceLineWithImages(content):
     imageEnd = content
@@ -53,82 +53,124 @@ def replaceLineWithItalic(content):
         content = preText + f"<em>{italicText}</em>" + italicEnd
     return content
 
-input_file = str(sys.argv[1])
-output_file = input_file.replace(".md", ".html")
-#output_file = str(sys.argv[2])
+def header_indent(header_depth_in):
+    return "\t"*(header_depth_in-1)
 
-indent_depth = ""
+def pageBoilerPlate(title, isHomePage, style_depth = 0):
+    indent_depth = ""
 
-isHomePage = (input_file[:len(input_file)-3]) == "index"
+    goBack = "../" * style_depth
 
-# Boilerplate
-html = "<!DOCTYPE html>\n"
-html += "<html lang=\"en\">\n"
-indent_depth += "\t"
-html += indent_depth + "<head>\n"
-indent_depth += "\t"
-html += indent_depth + "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />\n" #Adding Stylesheet
-#html += indent_depth + "<link rel=\"shortcut icon\" href=\"https://raw.githubusercontent.com/Hannah-Sloan/Teenage-Coven-Knights/master/docs/img/moon_chrome.ico\" type=\"image/x-icon\"/>\n" #Adding Favicon
-#html += indent_depth + "<link rel=\"icon\" href=\"https://raw.githubusercontent.com/Hannah-Sloan/Teenage-Coven-Knights/master/docs/img/moon_chrome.ico\" type=\"image/x-icon\"/>\n"
-html += indent_depth + "<title> "
-title = ""
-if(isHomePage): #Adding Title
-    html += "Home - Elysium Hunters" 
-    title = "Home"
-else:
+    # Boilerplate
+    html = "<!DOCTYPE html>\n"
+    html += "<html lang=\"en\">\n"
+    indent_depth += "\t"
+    html += indent_depth + "<head>\n"
+    indent_depth += "\t"
+    html += indent_depth + f"<link rel=\"stylesheet\" type=\"text/css\" href=\"{goBack}style.css\" />\n" #Adding Stylesheet
+    #html += indent_depth + "<link rel=\"shortcut icon\" href=\"https://raw.githubusercontent.com/Hannah-Sloan/Teenage-Coven-Knights/master/docs/img/moon_chrome.ico\" type=\"image/x-icon\"/>\n" #Adding Favicon
+    #html += indent_depth + "<link rel=\"icon\" href=\"https://raw.githubusercontent.com/Hannah-Sloan/Teenage-Coven-Knights/master/docs/img/moon_chrome.ico\" type=\"image/x-icon\"/>\n"
+    html += indent_depth + "<title> "
+    html += title + " - Elysium Hunters"
+    html += " </title>\n"
+    html += indent_depth + f"<meta name=\"description\" content=\"Elysium Hunters - {title}\" >\n"
+    html += indent_depth + "<meta name=\"keywords\" content=\"HTML, CSS, RPG, open, Elysium Hunters, Elysium, Hunters, rules-lite, sci-fi, scifi, indie\">\n"
+    html += indent_depth + "<meta name=\"author\" content=\"Hannah Ava Sloan\">\n"
+    html += indent_depth + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+    indent_depth = indent_depth.replace("\t", "", 1)
+    html += indent_depth + "</head>\n"
+
+    #Body
+    html += indent_depth + "<body>\n"
+    indent_depth += "\t"
+
+    html += indent_depth + f"<p><a href=\"https://github.com/Hannah-Sloan/Elysium-Hunters\">GITHUB</a>\n"
+    
+    if(not(isHomePage)):
+        html += indent_depth + f"<a href=\"{goBack}index.html\">HOME</a></p>\n"
+
+    html += indent_depth + "<main>\n"
+    indent_depth += "\t"
+
+    return html, indent_depth
+
+def endingBoilerPlate(html, indent_depth, input_file):
+    indent_depth = indent_depth.replace("\t", "", 1)
+    html += indent_depth + "</main>\n"
+
+    html += indent_depth + "<div id=\"license\"> <a rel=\"license\" href=\"https://github.com/Hannah-Sloan/Elysium-Hunters/blob/master/LICENSE.md\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-nc/4.0/88x31.png\" /></a><br /><span xmlns:dct=\"http://purl.org/dc/terms/\" href=\"http://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">The <b>text</b> contents of Elysium Hunters</span> is licensed under a <a rel=\"license\" href=\"https://github.com/Hannah-Sloan/Elysium-Hunters/blob/master/LICENSE.md\">Creative Commons Attribution-NonCommercial 4.0 International License</a>.</div>\n"
+
+    indent_depth = indent_depth.replace("\t", "", 1)
+    html += indent_depth + "</body>\n"
+    html += "</html>"
+
+    output_file = input_file.replace(".md", ".html")
+    output_file = output_file.replace("markdown\\", "docs\\")
+
+    print("CREATING PAGE: " + output_file)
+    path = os.path.dirname(f"{output_file}")
+    if not (os.path.exists(path)) and not(".html" in output_file):
+        os.makedirs(path)
+    # Write the output to a file
+    with open(f"{output_file}", "w", encoding="utf8") as f:
+        f.write(html)
+
+def createHomePage(path, pages, subfolders):
+    title = path.split("\\")[-1]
+
+    title = title.replace("_", " ")
+    title = title.upper()
+
+    isHomePage = False
+    if title == "MARKDOWN":
+        title = "HOME"
+        isHomePage = True
+    print("HOME PAGE: " + title)
+    
+    html, indent_depth = pageBoilerPlate(title, isHomePage)
+
+    html += indent_depth + f"<h1>{title}</h1>\n"
+
+    for subfolder in subfolders:
+        linkPath = subfolder[0] + ".html"
+        linkPath = os.path.join(path, linkPath).replace("markdown\\", "")
+        linkName = subfolder[0].split("\\")[-1]
+        linkName = linkName.replace("_", " ")
+        linkName = linkName.upper()
+        html += indent_depth + f"<p><a href={linkPath}>{linkName}</a></p>\n"
+
+    for page in pages:
+        pageName = page.replace("_", " ")
+        pageName = pageName.replace(".md", "")
+        pageName = pageName.upper()
+        page = page.replace(".md", ".html")
+        link = os.path.join(path, page).replace("markdown\\", "")
+        html += indent_depth + f"<p><a href={link}>{pageName}</a></p>\n"
+
+    if title == "HOME":
+        html += indent_depth + "<p>Read more about Elysium Hunters' Game License on Github: <a href=\"https://github.com/Hannah-Sloan/Elysium-Hunters/blob/master/LICENSE.md\">LICENSE</a></p>"
+
+    path = path + ".html"
+    path = path.replace("markdown.html", "markdown\\index.html")
+    endingBoilerPlate(html, indent_depth, path)
+
+def convert(input_file):
     title = (input_file[:len(input_file)-3])
     title = title.replace("_", " ")
     title = title.upper()
-    html += title + " - Elysium Hunters"
-html += " </title>\n"
-html += indent_depth + f"<meta name=\"description\" content=\"Elysium Hunters - {title}\" >\n"
-html += indent_depth + "<meta name=\"keywords\" content=\"HTML, CSS, RPG, open, Elysium Hunters, Elysium, Hunters, rules-lite, sci-fi, scifi, indie\">\n"
-html += indent_depth + "<meta name=\"author\" content=\"Hannah Ava Sloan\">\n"
-html += indent_depth + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
-indent_depth = indent_depth.replace("\t", "", 1)
-html += indent_depth + "</head>\n"
 
+    html, indent_depth = pageBoilerPlate(title, False, len(input_file.split("\\"))-2)
 
-#Body
-html += indent_depth + "<body>\n"
-indent_depth += "\t"
+    with open(f"{input_file}", "r", encoding="utf8") as f:
+        md = f.read()
 
-html += indent_depth + f"<p><a href=\"https://github.com/Hannah-Sloan/Elysium-Hunters\">GITHUB</a>\n"
-if(not(isHomePage)): #Adding Title
-    html += indent_depth + f"<a href=\"index.html\">HOME</a></p>\n"
+    counter = 0
 
-html += indent_depth + "<main>\n"
-indent_depth += "\t"
+    header_depth = 1
 
-with open(f"markdown\{input_file}", "r", encoding="utf8") as f:
-    md = f.read()
+    lines = md.split("\n")
+    curSection = False
 
-counter = 0
-
-header_depth = 1
-
-def header_indent(header_depth_in):
-  return "\t"*(header_depth_in-1)
-
-lines = md.split("\n")
-curSection = False
-
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir("markdown\\") if (isfile(join("markdown\\", f)) and ".md" in f)]
-
-if(isHomePage):
-    html += indent_depth + "<h1>Elysium Hunters</h1>\n"
-    for page in onlyfiles:
-        if(page == "index.md"): 
-            continue
-        pageName = page.replace("_", " ")
-        pageName = page.replace(".md", "")
-        pageName = pageName.upper()
-        page = page.replace(".md", ".html")
-        html += indent_depth + f"<p><a href={page}>{pageName}</a></p>\n"
-    html += indent_depth + "<p>Read more about Elysium Hunters' Game License on Github: <a href=\"https://github.com/Hannah-Sloan/Elysium-Hunters/blob/master/LICENSE.md\">LICENSE</a></p>"
-else:
     for line in lines:
         if line.startswith("#"):
             level = line.count("#")
@@ -157,19 +199,4 @@ else:
 
                 html += indent_depth + header_indent(header_depth+1) + f"<p>{line}</p>\n"
 
-indent_depth = indent_depth.replace("\t", "", 1)
-html += indent_depth + "</main>\n"
-
-html += indent_depth + "<div id=\"license\"> <a rel=\"license\" href=\"https://github.com/Hannah-Sloan/Elysium-Hunters/blob/master/LICENSE.md\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"https://i.creativecommons.org/l/by-nc/4.0/88x31.png\" /></a><br /><span xmlns:dct=\"http://purl.org/dc/terms/\" href=\"http://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">The <b>text</b> contents of Elysium Hunters</span> is licensed under a <a rel=\"license\" href=\"https://github.com/Hannah-Sloan/Elysium-Hunters/blob/master/LICENSE.md\">Creative Commons Attribution-NonCommercial 4.0 International License</a>.</div>\n"
-
-indent_depth = indent_depth.replace("\t", "", 1)
-html += indent_depth + "</body>\n"
-html += "</html>"
-
-import os
-path = os.path.dirname(f"docs\{output_file}")
-if not (os.path.exists(path)):
-    os.makedirs(path)
-# Write the output to a file
-with open(f"docs\{output_file}", "w", encoding="utf8") as f:
-    f.write(html)
+    endingBoilerPlate(html, indent_depth, input_file)
